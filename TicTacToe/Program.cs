11bf112969df747f28b3aa133c258054d26f1c86
jaguste.pranav <jaguste.pranav31@gmail.com>
2020-10-05 16:59:05 +0530

    [pranav] merge

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to TiTacToe Game!");
+            TicTacToeGame game = new TicTacToeGame();
+            game.CreateGame();
+            game.selectCharacter();
+            game.showBoard();
+            game.markCharacter();
+        }
+    }
+}
